31a3bb303fb4f6020ada2b2bc31c59dfcdfee66e
Lyriom <130936689+Lyriom@users.noreply.github.com>
2025-11-27 21:39:11 -0500

    se crean todsssssvfffgggggggo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Link from "next/link";
 import ProductTable from "@/components/ProductTable";
-
+import Image from "next/image"; 
 export default function Home() {
   return (
     <main style={{ padding: "20px" }}>
       <h1>Lista de Productos</h1>
-
+           <img 
+src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3N
+ jExNHE0YnUyNDF4OHdvYTYwaXh2eW9wOTBpemExZmthemk4NTh4Y
+ 3ljZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/xTiIzJ
+ SKB4l7xTouE8/giphy.gif" /> 
       {/* Botones de Login / Logout */}
       <div style={{ margin: "20px 0", display: "flex", gap: "10px" }}>
         <Link
--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,25 +1,19 @@
-import { createServerClient } from '@supabase/ssr'
-import { cookies } from 'next/headers'
+import { createServerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
 
-export async function createClient() {
-  const cookieStore = await cookies()
+export const createClient = () => {
+  const cookieStore = cookies();
 
   return createServerClient(
     process.env.SUPABASE_URL!,
     process.env.SUPABASE_ANON_KEY!,
     {
       cookies: {
-        getAll() {
-          return cookieStore.getAll()
-        },
-        setAll(cookiesToSet) {
-          try {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options)
-            )
-          } catch {}
+        get(name: string) {
+          return cookieStore.get(name)?.value;
         },
       },
     }
-  )
-}
+  );
+};
+
